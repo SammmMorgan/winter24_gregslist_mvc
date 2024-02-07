3f12e2f0f5ef26388e48b31ea820a573ca2e65e9
Sam Morgan <sam2morgan99@gmail.com>
2024-02-07 14:56:06 -0700

    file creation

--- app/router-config.js
+++ app/router-config.js
@@ -1,6 +1,7 @@
 import { CarsController } from "./controllers/CarsController.js";
 import { ExamplesController } from "./controllers/ExamplesController.js";
 import { HomeController } from "./controllers/HomeController.js";
+import { HousesController } from "./controllers/HousesController.js";
 import { Router } from "./utils/Router.js";
 
 
@@ -20,5 +21,10 @@ export const router = new Router([
     // NOTE filepath to our CarsView HTML file
     view: 'app/views/CarsView.html'
   },
+  {
+    path: '#/houses',
+    controllers: [HousesController],
+    view: 'app/views/HousesView.html'
+  }
   // TODO create route object for houses!
 ])
